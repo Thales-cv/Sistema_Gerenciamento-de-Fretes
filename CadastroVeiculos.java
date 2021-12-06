01a15d0107d788ab43a1f497b53368f8db64a0de
BrunoAvellar <93959830+BrunoAvellar@users.noreply.github.com>
2021-12-06 20:31:56 -0300

    2

--- CadastroVeiculos.java
+++ CadastroVeiculos.java
@@ -9,6 +9,7 @@ public class CadastroVeiculos {
 	}
 
 	public String listAno() {
+		
 		return null;
 	}
 
@@ -29,4 +30,36 @@ public class CadastroVeiculos {
 		return veiculos.add(veiculo);
 	}
 
+}
+public void listAlfa() { // mandar p thales 
+	Set<String> nomes = new TreeSet<String>();
+	for(int i=0; i<veiculo.size(); i++){
+		String nome = funcionarios.get(i).getNome();
+		nomes.add(nome);
+	}
+	
+	System.out.println(nomes);
+	
+	int n = nomes.size();
+
+	List<String> list = new ArrayList<String>(nomes);
+
+	for( String ele : list ){
+		System.out.print(ele + " " );
+	}
+
+	for(int i=0; i<list.size(); i++){
+		buscarNome(list.get(i));
+	}
+	
+}
+
+public Funcionario buscarNome(String nomeBuscado) {
+	for (int i = 0; i < funcionarios.size(); i++) {
+		if (funcionarios.get(i).getNome() == nomeBuscado) {
+			Funcionario a = funcionarios.get(i);
+			System.out.println(a.toString());
+		}
+	}
+	return null;
 }
